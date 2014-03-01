206056242659e8f2bc9dc31bd4eeed2aedcd612f
Lisa Strebl <d09096@student.htlwrn.ac.at>
2014-03-01 11:11:33 +0100

    Anmelden und Regestrierungs Formular erstellt.

--- joomla-cms/contiuni/index.php
+++ joomla-cms/contiuni/index.php
@@ -1,1 +1,23 @@
-TestTestTest
+<div class="anmelden">
+<h1 style="text-align:center; color:#F69F2B">ContiUNI</h1>
+	<div>
+		<div>
+			<label id="username-lbl">E-Mail Adresse *</label>						
+		</div>
+		<div>
+			<input type="email" required size="30" id="anmeldeemail" name="anmeldeemail" placeholder="E-Mail Adresse" value=" ">
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="password-lbl">Passwort *</label>
+		</div>
+		<div>
+			<input type="password" required size="30" id="anmeldepasswort" name="anmedldepasswort" placeholder="Passwort" value="">						
+		</div>
+	</div>		
+	<br>
+	<input type="submit" value="Anmelden">
+	<br>
+	<a href="/contiuni/registrieren.php" style="color:#F69F2B; text-decoration:none;">Registrieren</a>
+</div>
---
+++ joomla-cms/contiuni/registrieren.php
@@ -0,0 +1,57 @@
+<div class="registrieren">
+	<h2 style="text-align:center; color:#F69F2B">Registrieren</h2>
+	
+	<div>
+		<div>
+			<label id="vorname-lbl">Vorname *</label>						
+		</div>
+		<div>
+			<input type="text" required size="60" id="registrierenvorname" name="registrierenvorname" placeholder="Vorname" value="">
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="nachname-lbl">Nachname *</label>
+		</div>
+		<div>
+			<input type="text" required size="60" id="registrierennachname" name="registrierennachname" placeholder="Nachname" value="">						
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="passwort-lbl">Passwort *</label>						
+		</div>
+		<div>
+			<input type="password" required size="60" id="registrierenpasswort" name="registrierenpasswort" placeholder="Passwort" value="">
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="passwort2-lbl">Passwort wiederholen *</label>
+		</div>
+		<div>
+			<input type="password" required size="60" id="registrierenpasswort2" name="registrierenpasswort2" placeholder="Passwort wiederholen" value="">						
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="email-lbl">E-Mail Adresse *</label>						
+		</div>
+		<div>
+			<input type="email" required size="60" id="registrierenemail" name="registrierenemail" placeholder="E-Mail Adresse" value=" ">
+		</div>
+	</div>
+	<div>
+		<div>
+			<label id="email2-lbl">E-Mail Adresse wiederholen *</label>
+		</div>
+		<div>
+			<input type="email" required size="60" id="registrierenemail2" name="registrierenemail2" placeholder="E-Mail Adresse wiederholen" value="">						
+		</div>
+	</div>
+	<br>
+	<input type="submit" value="Registrieren">
+	<br>
+	<a href="/contiuni/index.php" style="color:#F69F2B; text-decoration:none;">Zurück zur Anmeldung</a>
+	
+</div>
